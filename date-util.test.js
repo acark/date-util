fef9efff0c2c71cef055bfb9a50972db74858ce0
acark <acar15k@gmail.com>
2023-04-20 16:12:43 +0300

    test file updated with the new class name

--- date-util.test.js
+++ date-util.test.js
@@ -1,98 +1,98 @@
-const DateUtil = require('./date-util');
+const Datex = require('./date-util');
 
-describe('DateUtil', () => {
-  let dateUtil;
+describe('Datex', () => {
+  let datex;
 
   beforeEach(() => {
-    dateUtil = new DateUtil('2023-03-29');
+    datex = new Datex('2023-03-29');
   });
 
   test('startOfDay', () => {
-    const start = dateUtil.startOfDay();
+    const start = datex.startOfDay();
     expect(start.toISOString()).toBe('2023-03-29T00:00:00.000Z');
   });
 
   test('endOfDay', () => {
-    const end = dateUtil.endOfDay();
+    const end = datex.endOfDay();
     expect(end.toISOString()).toBe('2023-03-29T23:59:59.999Z');
   });
 
   test('isLeapYear', () => {
-    expect(dateUtil.isLeapYear()).toBe(false);
+    expect(datex.isLeapYear()).toBe(false);
   });
 
   test('daysInMonth', () => {
-    expect(dateUtil.daysInMonth()).toBe(31);
+    expect(datex.daysInMonth()).toBe(31);
   });
 
   test('isSameDayWith', () => {
     const anotherDate = new Date('2023-03-29');
-    expect(dateUtil.isSameDayWith(anotherDate)).toBe(true);
+    expect(datex.isSameDayWith(anotherDate)).toBe(true);
   });
 
   test('isToday', () => {
     const today = new Date();
-    expect(dateUtil.isToday()).toBe(dateUtil.isSameDayWith(today));
+    expect(datex.isToday()).toBe(datex.isSameDayWith(today));
   });
 
   test('toMySQLDateString', () => {
-    expect(dateUtil.toMySQLDateString()).toBe('2023-03-29');
+    expect(datex.toMySQLDateString()).toBe('2023-03-29');
   });
 
   test('toMySQLDateTimeString', () => {
-    expect(dateUtil.toMySQLDateTimeString()).toBe('2023-03-29T00:00:00');
+    expect(datex.toMySQLDateTimeString()).toBe('2023-03-29T00:00:00');
   });
 
   test('dateRangeForToday', () => {
-    const range = dateUtil.dateRangeForToday('created_at');
+    const range = datex.dateRangeForToday('created_at');
     expect(range).toBe("created_at BETWEEN '2023-03-29' AND '2023-03-29'");
   });
 
   test('isWeekend', () => {
-    expect(dateUtil.isWeekend()).toBe(false);
+    expect(datex.isWeekend()).toBe(false);
   });
 
   test('isWeekday', () => {
-    expect(dateUtil.isWeekday()).toBe(true);
+    expect(datex.isWeekday()).toBe(true);
   });
 
   test('differenceInDays', () => {
     const anotherDate = new Date('2023-04-04');
-    expect(dateUtil.differenceInDays(anotherDate)).toBe(6);
+    expect(datex.differenceInDays(anotherDate)).toBe(6);
   });
 
   test('isSameMonth', () => {
     const anotherDate = new Date('2023-03-01');
-    expect(dateUtil.isSameMonth(anotherDate)).toBe(true);
+    expect(datex.isSameMonth(anotherDate)).toBe(true);
   });
 
   test('addSeconds', () => {
-    const result = dateUtil.addSeconds(30);
+    const result = datex.addSeconds(30);
     expect(result.getSeconds()).toBe(30);
   });
 
   test('addMinutes', () => {
-    const result = dateUtil.addMinutes(15);
+    const result = datex.addMinutes(15);
     expect(result.getMinutes()).toBe(15);
   });
 
   test('addHours', () => {
-    const result = dateUtil.addHours(4);
+    const result = datex.addHours(4);
     expect(result.getHours()).toBe(4);
   });
 
   test('addDays', () => {
-    const result = dateUtil.addDays(7);
+    const result = datex.addDays(7);
     expect(result.getDate()).toBe(5);
   });
 
   test('addMonths', () => {
-    const result = dateUtil.addMonths(2);
+    const result = datex.addMonths(2);
     expect(result.getMonth()).toBe(5);
   });
 
   test('addYears', () => {
-    const result = dateUtil.addYears(5);
+    const result = datex.addYears(5);
     expect(result.getFullYear()).toBe(2028);
   });
 
